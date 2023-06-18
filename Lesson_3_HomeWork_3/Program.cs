c11edaeae87ea2d3b9641d433374a7e109847595
Kostya <ashirokovkonstantin@gmail.com>
2023-06-18 15:20:29 +0500

    Create lesson 3 homework 3

---
+++ Lesson_3_HomeWork_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson_3_HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Prompt (string message)
+            {
+                Console.WriteLine(message);
+                return int.Parse(Console.ReadLine());
+            }
+
+            int x = Prompt("Введите число: ");
+            int i = 1;
+            while (i<=x)
+            {
+                Console.Write(Math.Pow(i, 3));
+                if (Math.Pow(i, 3) != Math.Pow(x, 3))
+                Console.Write(", ");
+                i++;
+            }
+
+        }
+    }
+}
