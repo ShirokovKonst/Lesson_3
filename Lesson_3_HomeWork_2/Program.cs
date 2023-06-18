8a4484309adb3ebc351fb954ecc27566323242e7
Kostya <ashirokovkonstantin@gmail.com>
2023-06-18 15:01:29 +0500

    Create lesson 3 homework 2

---
+++ Lesson_3_HomeWork_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson_3_HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double Prompt (string message)
+            {
+                Console.WriteLine(message);
+                return double.Parse(Console.ReadLine());
+            }
+            double Distance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+            {
+                double res = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+                return res;
+            }
+
+            Console.WriteLine(Distance3D(Prompt("Введите X1: "), Prompt("Введите Y11: "), Prompt("Введите Z1: "), Prompt("Введите X2: "), Prompt("Введите Y2: "), Prompt("Введите Z2: ")));
+        }
+    }
+}
